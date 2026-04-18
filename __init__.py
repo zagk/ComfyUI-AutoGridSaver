9f81a68da244be95645960c3a3b4e0ef67d3b721
zagk <junvin@gmail.com>
2026-04-18 19:57:50 +0900

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,11 +1,14 @@
 from .auto_grid_saver import AutoGridSaver
+from .metadata_extractor import MetadataExtractor # 새로 만든 파일 임포트
 
 NODE_CLASS_MAPPINGS = {
-    "AutoGridSaver": AutoGridSaver
+    "AutoGridSaver": AutoGridSaver,
+    "MetadataExtractor": MetadataExtractor
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "AutoGridSaver": "Auto Grid Saver zk"
+    "AutoGridSaver": "Auto Grid Saver zk",
+    "MetadataExtractor": "Metadata Extractor zk"
 }
 
 WEB_DIRECTORY = "./web"
